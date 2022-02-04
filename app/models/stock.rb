662291e401ed0c727dc15a5512d5f222b1c9d640
ahartfelder <ahartfelder@gmail.com>
2022-02-04 15:44:59 -0300

    set user_stocks

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,10 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+  before_save { self.ticker = ticker.upcase }
+
+  validates :name, :ticker, presence: true
+
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
       publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key], 
@@ -9,5 +15,9 @@ class Stock < ApplicationRecord
     rescue => exception
       nil
     end
+  end
+
+  def self.check_db(ticker)
+    where(ticker: ticker).first
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :user_stocks, only: :create
+  get 'search_stock', to: 'stocks#search'
+  get 'my_portfolio', to: 'users#my_portfolio'
   devise_for :users
   root 'welcome#index'
-  get 'my_portfolio', to: 'users#my_portfolio'
-  get 'search_stock', to: 'stocks#search'
 end
