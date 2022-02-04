ace09449659685d4515d60661625cf97d01aebd4
ahartfelder <ahartfelder@gmail.com>
2022-02-04 18:37:45 -0300

    added limitation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :user_stocks, only: :create
+  resources :user_stocks, only: [:create, :destroy]
   get 'search_stock', to: 'stocks#search'
   get 'my_portfolio', to: 'users#my_portfolio'
   devise_for :users
