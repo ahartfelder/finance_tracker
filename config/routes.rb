1710ba0bfd49150f6ad4e08ceef46b6afa906db7
ahartfelder <ahartfelder@gmail.com>
2022-02-08 09:17:05 -0300

    ordered routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :user_stocks, only: [:create, :destroy]
-  resources :friendships, only: [:create, :destroy]
-  resources :users, only: :show
-  get 'search_friend', to: 'users#search'
+  root 'welcome#index'
+  devise_for :users
+  get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
   get 'my_friends', to: 'users#my_friends'
-  get 'my_portfolio', to: 'users#my_portfolio'
-  devise_for :users
-  root 'welcome#index'
+  get 'search_friend', to: 'users#search'
+  resources :users, only: :show
+  resources :user_stocks, only: [:create, :destroy]
+  resources :friendships, only: [:create, :destroy]
 end
